1c6f23c7eb072750c67eb6be022983fdfea08dc5
wisaratham <worakarn.isaratham@agoda.com>
2023-12-02 15:48:56 +0700

    a bit shorter

--- 2023/ruby/p02.rb
+++ 2023/ruby/p02.rb
@@ -6,7 +6,6 @@ games = input.map{|line|
   res = parts[1..-1].map{|x| 
     comps = x.strip.split(/[, ]+/)
     h = Hash[*comps.reverse]
-    h.default = "0"
     [h["red"].to_i, h["green"].to_i, h["blue"].to_i]
   }
   [id, res]
@@ -20,8 +19,5 @@ p games.map{|id, parts|
 
 # part 2
 p games.map{|id, parts|
-  r = parts.map{|c| c[0]}.max
-  g = parts.map{|c| c[1]}.max
-  b = parts.map{|c| c[2]}.max
-  r*g*b
+  (0..2).map{|i| parts.map{|c| c[i]}.max}.reduce(:*)
 }.sum
